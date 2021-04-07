c8d470cd3224b677da3162a650375d486cfc27cf
Caitlin McCann <toomanycats@witchlounge.ca>
2021-04-06 20:42:50 -0700

    covariance & contravariance

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+class OrangeFruit: Fruit {
+  public OrangeFruit() {
+    Name = "Orange";
+  }
+}
+
+class RedFruit : Fruit {
+  public RedFruit () {
+    Name = "Apple";
+  }
+}
+
+class Fruit {
+  public string Name { get; set; }
+}
+
+class Program
+{
+    public static void Main()
+    {
+      Func<Fruit, Fruit> ToUpper = f => { f.Name = f.Name.ToUpper(); return f; };
+      // string[] fruit = { "apple", "orange", "banana", "pear", "cherry" };
+
+      IEnumerable<Fruit> fruit = new List<Fruit>() {  new RedFruit(), new OrangeFruit(), ToUpper(new OrangeFruit()) };
+
+      foreach(var f in fruit) {
+        Console.WriteLine(f.Name);
+      }
+    }
+}
